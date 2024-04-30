8ac66ea5a6e93881d5f1e247482ac523560f36fc
Luke Goodall <ljgoodall2001@gmail.com>
2024-04-30 09:19:40 +0200

    Added time to the MCPi.c program

---
+++ Prac1/MCPi.c
@@ -0,0 +1,43 @@
+/* Program to compute Pi using Monte Carlo methods */
+/* Compile with gcc mcpi.c -o mcp */
+/* Run with mcp */
+
+#include <time.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#define SEED 35791246
+
+int main(int argc, char* argv)
+{
+   clock_t start, end;
+   double exec_time;
+   start = clock(); //begin timing
+
+   int niter=0;
+   double x,y;
+   int i,count=0; /* # of points in the 1st quadrant of unit circle */
+   double z;
+   double pi;
+
+   printf("Enter the number of iterations used to estimate pi: ");
+   scanf("%d",&niter);
+
+   /* initialize random numbers */
+   srand(SEED);
+   count=0;
+   for ( i=0; i<niter; i++) {
+      x = (double)rand()/RAND_MAX;
+      y = (double)rand()/RAND_MAX;
+      z = x*x+y*y;
+      if (z<=1) count++;
+      }
+   pi=(double)count/niter*4;
+   printf("# of trials= %d , estimate of pi is %g \n",niter,pi);
+   end = clock();
+   exec_time = ((double) (end - start) * 1000) / CLOCKS_PER_SEC;
+   printf("Execution time: %f ms\n", exec_time);
+   return 0;
+}
